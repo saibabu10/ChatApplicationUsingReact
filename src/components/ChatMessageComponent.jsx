ed23c810047e6753622f038678f8efc7a4e81950
sai babu <saibabu@sais-MacBook-Air-3.local>
2024-03-12 09:04:46 +0530

    Add.ChatMessageComponent functionality is added

--- src/components/ChatMessageComponent.jsx
+++ src/components/ChatMessageComponent.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-function ChatMessageComponent({text}) {
+function ChatMessageComponent({text,logo,email,user}) {
   return (
-    <div>{text}</div>
+    <div className={`d-flex ${email===user.email && "justify-content"}`}>
+      {
+        user.email===email ? (
+          <span className='message-right'>
+            <span className='message-text'>{text}</span>
+            <img src={logo} alt="logo" className='logo-icon'/>
+          </span>
+        ) : (
+          <span className='message-left'>
+          <img src={logo} alt="logo" className='logo-icon'/>
+          <span className='message-text'>{text}</span>
+        </span>
+        )
+      }
+    </div>
   )
 }
 
